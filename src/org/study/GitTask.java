1bc2eff339fc9fb541241be9088fdd40b4af09cd
saral7200 <krsaralkumar@gmail.com>
2022-11-10 10:23:34 +0530

    GitTask

---
+++ src/org/study/GitTask.java
@@ -0,0 +1,27 @@
+package org.study;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GitTask {
+	public static void main(String[] args) {
+		List<String> li = new ArrayList<>();
+		li.add("saral");
+		li.add("karthi");
+		li.add("janakiraman");
+		li.add("shanmugam");
+		li.add("mani");
+		li.add("siva");
+		li.add("william");
+		li.add("prakash");
+		li.add("sultan");
+		
+		System.out.println(li);
+	for (int i = 0; i < li.size(); i++) {
+		String string = li.get(i);
+		System.out.println(string);
+	}	
+	li.remove(6);
+	System.out.println(li);
+	}
+}
